01d585691b05498090b8a60ba4516b8bdd308d78
vojaso82 <vojislavzaja@gmail.com>
2023-01-09 16:42:28 -0500

    added axios req

--- server/index.js
+++ server/index.js
@@ -1,5 +1,38 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localHost",
+  password: "password",
+  database: "employeeSystem",
+});
+
+app.post("/create", (req, res) => {
+  console.log("req.body", req.body);
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values inserted");
+      }
+    }
+  );
+});
 
 app.listen(3001, () => {
   console.log("Successfully conntected to server");
