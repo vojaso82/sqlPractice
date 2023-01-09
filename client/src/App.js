5681767397d556299adbb6c11fcd776b34dbbe55
vojaso82 <vojislavzaja@gmail.com>
2023-01-09 15:33:50 -0500

    initial

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+        />
+        <label>Position:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPosition(e.target.value);
+          }}
+        />
+        <label>Wage (year):</label>
+        <input
+          type="number"
+          onChange={(e) => {
+            setWage(e.target.value);
+          }}
+        />
+        <button>Add Employee</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
